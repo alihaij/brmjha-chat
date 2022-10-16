b880f5be54c78fe1e2c5d47a71968b9a67179b87
alihaij <a3haij@gmail.com>
2022-10-16 06:51:34 +0300

    add auth by email and password

---
+++ lib/auth_form.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  AuthForm(this.submitFn);
+  final void Function(
+    String email,
+    String password,
+    String user,
+    bool isLogin,
+    BuildContext context,
+  ) submitFn;
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String? _userEmail;
+  String? _userName;
+  String? _userPassword;
+  void _trySubmit() {
+    FocusScope.of(context).unfocus();
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+      widget.submitFn(
+        _userEmail!.trim(),
+        _userPassword!.trim(),
+        _userName!.trim(),
+        _isLogin,
+        context,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Please Enter valid Email address.';
+                    } else {
+                      return null;
+                    }
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(labelText: 'Email address'),
+                  onSaved: (value) {
+                    _userEmail = value;
+                  }, // InputDecoration
+                ),
+                if (!_isLogin)
+                  TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 4) {
+                        return ' Please Enter User Name more than 4 character';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: const InputDecoration(labelText: 'Username'),
+                    onSaved: (value) {
+                      _userName = value;
+                    },
+                  ), // TextFormField
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 7) {
+                      return 'Password must be at least 7 character long';
+                    }
+                  },
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  onSaved: (value) {
+                    _userPassword = value;
+                  },
+                  obscureText: true,
+                ), // TextFormField
+                const SizedBox(height: 12),
+                ElevatedButton(
+                  child: Text(_isLogin ? 'Login' : 'Signup'),
+                  onPressed: () {
+                    _trySubmit();
+                  },
+                ),
+                TextButton(
+                  child: Text(_isLogin
+                      ? 'Create new account'
+                      : 'I already have account'),
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin;
+                    });
+                  },
+                ), // TextFormField
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/auth_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _fireAuth = FirebaseAuth.instance;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String user,
+    bool isLogin,
+    BuildContext context,
+  ) async {
+    try {
+      UserCredential userCredential;
+      if (isLogin) {
+        userCredential = await _fireAuth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        userCredential = await _fireAuth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        if (!isLogin) {
+          FirebaseFirestore.instance
+              .collection('user')
+              .add(({'username': user, 'email': email}));
+        }
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.toString()),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: AuthForm(_submitAuthForm));
+  }
+}
